cded6887a5d6dc490674eb1ce288672d1a0a3b99
Aryan Jain <aryanjain@Aryans-MacBook-Pro.local>
2025-12-29 18:22:45 +0530

    add option to select 500

--- src/app/admin/campaigns/contacts/page.tsx
+++ src/app/admin/campaigns/contacts/page.tsx
@@ -259,6 +259,7 @@ export default function ContactsPage() {
                 <option value={10}>10</option>
                 <option value={50}>50</option>
                 <option value={100}>100</option>
+                <option value={500}>500</option>
                 <option value={1000}>1000</option>
               </select>
             </div>
